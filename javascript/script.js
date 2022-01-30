bafc708e0842e2f1593f0101c51ccf189e389504
Camila Garnero <camilagarnero10@gmail.com>
2022-01-30 16:59:32 -0300

    simulador.interactivo.quiz

--- javascript/script.js
+++ javascript/script.js
@@ -1,50 +1,24 @@
-//Condicionales
-let numero = prompt ("Introduce el número del mes que quieres saber");
-let estacion;
+function verificarRespuestas(){
 
-if( mes == 1 || mes == 2 || mes == 12){
-    estacion = "Verano";
-    alert("Es la estación de Verano");
-}
-else if( mes == 3 || mes == 4 || mes == 5){
-    estacion = "Otoño";
-    alert("Es la estación de Otoño");
-}
-else if( mes == 6 || mes == 7 || mes == 8){
-    estacion = "Invierno";
-    alert("Es la estación de Invierno");
-}
-else if ( mes == 9 || mes == 10 || mes == 11){
-    estacion = "Primavera";
-    alert("Es la estación de Primavera");
-}else {
-    estacion = "Valor Incorrecto";
-}
-console.log(estacion);
+    var total = 8;
+    var puntos = 0;
 
-//Ciclo For
-for(let contador = 0; contador <= 10; contador++){
-    if( contador % 2 !== 0){
-        continue;
-    }else {
-        console.log(contador);
+    var myForm = document.forms["quizForm"];
+    var respuestas = ["c","a","c","b","d","a","d","b"];
+
+    for(var i = 1; i <= total;i++){
+        if(myForm["p" + i].value === null || myForm["p" + i].value === ''){
+            alert("Por favor responde a la pregunta" + i);
+            return false;
+        }else{
+            if(myForm["p" + i].value === respuestas[i - 1]){
+                puntos++;
+            }
+        }
     }
+
+    var resultado = document.getElementById('resultado');
+    resultado.innerHTML = '<h3>Obtuviste <span>'+puntos+'</span> de <span>'+total+' puntos</span></h3>';
+
+    return false;
 }
-
-//Ciclo While
-let contador = 0;
-
-while( contador < 3){
-    console.log(contador);
-    contador++;
-}
-console.log("Fin del Ciclo")
-
-//Ciclo Do...While
-let contador = 0
-
-do{
-    console.log(contador);
-    contador++;
-}while(contador < 3);
-console.log("Fin del Ciclo");
